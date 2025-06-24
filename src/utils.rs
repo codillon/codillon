cea56bb4041561ee5e8c634f1ddbe24d2b2e6e99
emmasudo <183161889+emmasudo1@users.noreply.github.com>
2025-06-24 10:48:33 -0700

    add well-formed function

---
+++ src/utils.rs
@@ -0,0 +1,23 @@
+use wast::Wat;
+use wast::parser::{self, ParseBuffer};
+
+/// Decides if a given string is a well-formed text-format Wasm instruction
+/// 
+/// Wraps instruction in function and parses using wast parser
+///
+/// # Parameters
+/// s: A string slice representing a Wasm instruction
+///
+/// # Returns
+/// true: if the instruction is syntactically well-formed; false otherwise
+pub fn is_well_formed_instr(s: &str) -> bool {
+    let wat = "(func ".to_string() + s + ")";
+    let buf = match ParseBuffer::new(&wat) {
+        Ok(b) => b,
+        Err(_) => return false,
+    };
+    match parser::parse::<Wat>(&buf) {
+        Ok(_) => return true,
+        Err(_) => return false,
+    }
+}
