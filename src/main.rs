0ea81920e1c89df24030a10c2810d2fca044234e
emmasudo <183161889+emmasudo1@users.noreply.github.com>
2025-06-24 10:48:33 -0700

    add well-formed function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+mod utils;
 use leptos::prelude::*;
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    is_well_formed_instr_test();
+    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> });
+}
+
+///testing is_well_formed_instr function
+///
+///prints sample instruction and bool
+fn is_well_formed_instr_test() {
+    let sample_instr = "i32.add";
+    println!("\"{}\" is well-formed: {}", sample_instr, utils::is_well_formed_instr(sample_instr));
 }
