0a27f122c6c090e7ffdeb8bff3d1bfce4f323533
emmasudo <183161889+emmasudo1@users.noreply.github.com>
2025-06-24 10:48:33 -0700

    add well-formed function

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
-use wast::Wat;
+use wast::core::Instruction;
 use wast::parser::{self, ParseBuffer};
 
 /// Decides if a given string is a well-formed text-format Wasm instruction
 ///
-/// Wraps instruction in function and parses using wast parser
+/// Uses ParseBuffer to convert string into single instruction buffer and parser to parse buffer
 ///
 /// # Parameters
 /// s: A string slice representing a Wasm instruction
@@ -11,12 +11,19 @@ use wast::parser::{self, ParseBuffer};
 /// # Returns
 /// true: if the instruction is syntactically well-formed; false otherwise
 pub fn is_well_formed_instr(s: &str) -> bool {
-    let wat = "(func ".to_string() + s + ")";
-    let buf = match ParseBuffer::new(&wat) {
+    //manually check for empty line and line comments (block comments not supported)
+    if s.is_empty() || s.starts_with(";;") {
+        return true;
+    }
+    //manually check for spaces at the beginning and end
+    if s.starts_with(" ") || s.ends_with(" ") {
+        return false;
+    }
+    let buf = match ParseBuffer::new(s) {
         Ok(b) => b,
         Err(_) => return false,
     };
-    parser::parse::<Wat>(&buf).is_ok()
+    parser::parse::<Instruction>(&buf).is_ok()
 }
 
 #[cfg(test)]
@@ -28,9 +35,21 @@ mod tests {
         //well-formed instructions
         assert!(is_well_formed_instr("i32.add"));
         assert!(is_well_formed_instr("i32.const 5"));
-        //not well-formed instructions
+        //not well-formed "instructions"
         assert!(!is_well_formed_instr("i32.bogus"));
         assert!(!is_well_formed_instr("i32.const"));
         assert!(!is_well_formed_instr("i32.const x"));
+        //not well-formed "instructions": multiple instructions per line, folded instructions,
+        //spaces before and after
+        assert!(!is_well_formed_instr("i32.const 4 i32.const 5"));
+        assert!(!is_well_formed_instr("(i32.const 4)"));
+        assert!(!is_well_formed_instr(
+            "(i32.add (i32.const 4) (i32.const 5))"
+        ));
+        assert!(!is_well_formed_instr("    i32.const 5"));
+        assert!(!is_well_formed_instr("i32.const 5     "));
+        //comments and empty lines are well-formed
+        assert!(is_well_formed_instr(";;Hello"));
+        assert!(is_well_formed_instr(""));
     }
 }
