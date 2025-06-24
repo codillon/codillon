a1365c3c55dd2778be95ed9b1f47bc6dd4da399c
emmasudo <183161889+emmasudo@users.noreply.github.com>
2025-06-24 13:04:41 -0700

    add well-formed function (#8)
    * add well-formed function
    
    * failing CI
    
    * still fixing CI
    
    * added unit tests
    
    * fixed CI errors
    
    * add well-formed function
    
    * failing CI
    
    * still fixing CI
    
    * added unit tests
    
    * fixed CI errors
    
    * Work around https://github.com/trunk-rs/trunk/issues/959 / https://github.com/trunk-rs/trunk/issues/951
    
    * Re-add body of main() function to work around https://github.com/trunk-rs/trunk/issues/959
    
    * Restore missing `use`
    
    * format
    
    ---------
    
    Co-authored-by: emmasudo <183161889+emmasudo1@users.noreply.github.com>
    Co-authored-by: Keith Winstein <keithw@cs.stanford.edu>

---
+++ src/utils.rs
@@ -0,0 +1,36 @@
+use wast::Wat;
+use wast::parser::{self, ParseBuffer};
+
+/// Decides if a given string is a well-formed text-format Wasm instruction
+///
+/// Wraps instruction in function and parses using wast parser
+///
+/// # Parameters
+/// s: A string slice representing a Wasm instruction
+///
+/// # Returns
+/// true: if the instruction is syntactically well-formed; false otherwise
+pub fn is_well_formed_instr(s: &str) -> bool {
+    let wat = "(func ".to_string() + s + ")";
+    let buf = match ParseBuffer::new(&wat) {
+        Ok(b) => b,
+        Err(_) => return false,
+    };
+    parser::parse::<Wat>(&buf).is_ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_well_formed_instr() {
+        //well-formed instructions
+        assert!(is_well_formed_instr("i32.add"));
+        assert!(is_well_formed_instr("i32.const 5"));
+        //not well-formed instructions
+        assert!(!is_well_formed_instr("i32.bogus"));
+        assert!(!is_well_formed_instr("i32.const"));
+        assert!(!is_well_formed_instr("i32.const x"));
+    }
+}
