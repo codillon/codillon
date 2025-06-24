c4cd7c4088c46a18c61df65693dcb66d5fcf06e0
Jiahao Zhang <33959051+jhzhics@users.noreply.github.com>
2025-06-24 12:30:32 -0700

    Add textbox (#10)
    * Add textbox
    
    * Display the text in better format

---
+++ src/frontend/index/textbox.rs
@@ -0,0 +1,20 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn textbox() -> impl IntoView {
+    let (text, set_text) = signal("".to_string());
+
+    view! {
+        <input
+            type="text"
+            prop:value=text
+            on:input=move |ev| {
+                set_text.set(event_target_value(&ev));
+            }
+            placeholder="Enter Some Text"
+        />
+        <p>
+            "The text is: " {text}
+        </p>
+    }
+}
