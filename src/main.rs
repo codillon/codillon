fffdd8cf39df3f21c445110fce1967c5efc07742
emmasudo <183161889+emmasudo1@users.noreply.github.com>
2025-06-25 10:37:58 -0700

    Fix redefined module

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use leptos::prelude::*;
-mod utils;
 use codillon::frontend::index;
-mod utils;
 
 fn main() {
-    utils::is_well_formed_instr("(i32.const 5)");
     leptos::mount::mount_to_body(index::textbox::Textbox);
 }
+
