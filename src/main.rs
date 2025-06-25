55e1e3ff462fc4e8ac52f986dda80d12d93ca804
emmasudo <183161889+emmasudo1@users.noreply.github.com>
2025-06-24 22:06:59 -0700

    fine grained improvements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use leptos::prelude::*;
 mod utils;
 use codillon::frontend::index;
+mod utils;
 
 fn main() {
+    utils::is_well_formed_instr("(i32.const 5)");
     leptos::mount::mount_to_body(index::textbox::Textbox);
 }
