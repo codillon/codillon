161f34561438a80e1d7dadb55bff89cd764b7d5f
emmasudo <183161889+emmasudo@users.noreply.github.com>
2025-06-25 16:19:46 -0700

    Syntax Checker Enforces Tighter Requirements on Instruction (#21)
    * add well-formed function
    
    * Restore missing `use`
    
    * fine grained improvements
    
    * Fix redefined module
    
    * formatting
    
    * fixed comments and spaces
    
    * uses split
    
    * uses expect
    
    * it's a lifestyle: better msg/one line
    
    ---------
    
    Co-authored-by: emmasudo <183161889+emmasudo1@users.noreply.github.com>
    Co-authored-by: Keith Winstein <keithw@cs.stanford.edu>

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
-use wast::Wat;
+use wast::core::Instruction;
 use wast::parser::{self, ParseBuffer};
 
 /// Decides if a given string is a well-formed text-format Wasm instruction
 ///
-/// Wraps instruction in function and parses using wast parser
+/// Uses wast ParseBuffer to convert string into buffer and wast parser to parse buffer as Instruction
 ///
 /// # Parameters
 /// s: A string slice representing a Wasm instruction
@@ -11,12 +11,21 @@ use wast::parser::{self, ParseBuffer};
 /// # Returns
 /// true: if the instruction is syntactically well-formed; false otherwise
 pub fn is_well_formed_instr(s: &str) -> bool {
-    let wat = "(func ".to_string() + s + ")";
-    let buf = match ParseBuffer::new(&wat) {
+    //get rid of comments and spaces (clippy says not to use nth...)
+    let s = s
+        .split(";;")
+        .next()
+        .expect("Split unexpectedly produced empty iterator")
+        .trim();
+    //manually check for empty line
+    if s.is_empty() {
+        return true;
+    }
+    let buf = match ParseBuffer::new(s) {
         Ok(b) => b,
         Err(_) => return false,
     };
-    parser::parse::<Wat>(&buf).is_ok()
+    parser::parse::<Instruction>(&buf).is_ok()
 }
 
 #[cfg(test)]
@@ -28,9 +37,21 @@ mod tests {
         //well-formed instructions
         assert!(is_well_formed_instr("i32.add"));
         assert!(is_well_formed_instr("i32.const 5"));
-        //not well-formed instructions
+        //not well-formed "instructions"
         assert!(!is_well_formed_instr("i32.bogus"));
         assert!(!is_well_formed_instr("i32.const"));
         assert!(!is_well_formed_instr("i32.const x"));
+        //not well-formed "instructions": multiple instructions per line, folded instructions
+        assert!(!is_well_formed_instr("i32.const 4 i32.const 5"));
+        assert!(!is_well_formed_instr("(i32.const 4)"));
+        assert!(!is_well_formed_instr(
+            "(i32.add (i32.const 4) (i32.const 5))"
+        ));
+        //spaces before and after, comments, and empty lines are well-formed
+        assert!(is_well_formed_instr("    i32.const 5"));
+        assert!(is_well_formed_instr("i32.const 5     "));
+        assert!(is_well_formed_instr(";;Hello"));
+        assert!(is_well_formed_instr("i32.const 5   ;;this is a const"));
+        assert!(is_well_formed_instr(""));
     }
 }
