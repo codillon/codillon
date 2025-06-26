961925d737e3d43e00fdfba9af56b6dd63737292
Keith Winstein <keithw@cs.stanford.edu>
2025-06-26 09:58:43 -0700

    boxlist.rs: make id uniqueness local to EditorBuffer
    This avoids creating a global thread-safe counter.

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -4,21 +4,6 @@ use leptos::prelude::*;
 mod boxlist;
 mod textbox;
 
-/// This Global Id Allocator provides unique ids for objects.
-/// The id may be used by leptos to for handling objects dynamically.
-/// The exact number of id is not guaranteed
-static GLOB_ID_ALLOCATOR: GlobalIdAllocator =
-    GlobalIdAllocator(std::sync::atomic::AtomicUsize::new(0));
-
-#[derive(Debug)]
-struct GlobalIdAllocator(std::sync::atomic::AtomicUsize);
-
-impl GlobalIdAllocator {
-    pub fn new_id(&self) -> usize {
-        self.0.fetch_add(1, std::sync::atomic::Ordering::Relaxed)
-    }
-}
-
 #[component]
 pub fn App() -> impl IntoView {
     view! { <boxlist::Boxlist /> }
