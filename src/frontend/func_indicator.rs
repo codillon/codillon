bbec4b7bfd1b18086ace5d39a13c320c3f1c0cee
emmasudo <183161889+emmasudo1@users.noreply.github.com>
2025-06-26 15:01:42 -0700

    wip

--- src/frontend/func_indicator.rs
+++ src/frontend/func_indicator.rs
@@ -2,6 +2,15 @@ use super::EditorBuffer;
 use crate::utils::is_well_formed_func;
 use leptos::prelude::*;
 
+/// Single indicator for function well-formedness
+///
+/// Checks if the entire function is syntactically well-formed and if each instruction is well-formed
+///
+/// # Parameters
+/// editor_buffer: A signal that holds the current state of the EditorBuffer
+///
+/// # Returns
+/// A view that displays a happy emoji if the function is well-formed, otherwise a sad emoji
 #[component]
 pub fn FuncIndicator(editor_buffer: ReadSignal<EditorBuffer>) -> impl IntoView {
     view! {
