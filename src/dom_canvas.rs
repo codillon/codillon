882f22ea07e337f7d78bdf51fb79ca69d3fc3609
Keith Winstein <keithw@cs.stanford.edu>
2026-06-04 16:09:38 -0700

    Show/hide canvas based on whether graphics API is *ever* used

--- src/dom_canvas.rs
+++ src/dom_canvas.rs
@@ -31,12 +31,16 @@ impl DomCanvas {
             extent: (-1.0, 1.0, -1.0, 1.0),
             point_radius: 3.0,
         };
-        ret.reset();
+        ret.reset(false);
         Ok(ret)
     }
 
-    pub fn reset(&mut self) {
-        self.elem.remove_attribute("class");
+    pub fn reset(&mut self, visible: bool) {
+        if visible {
+            self.elem.set_attribute("class", "graph-canvas");
+        } else {
+            self.elem.remove_attribute("class");
+        }
         self.render(&Some(Action::Clear));
         self.render(&Some(Action::Color(0, 0, 0)));
         self.render(&Some(Action::Radius(3.0)));
@@ -52,7 +56,6 @@ impl DomCanvas {
             match *action {
                 Clear => self.context.clear_rect(0.0, 0.0, WIDTH, HEIGHT),
                 Point(x, y) => {
-                    self.elem.set_attribute("class", "graph-canvas");
                     let (xmin, xmax, ymin, ymax) = extent;
                     let pixel_x = (x - xmin) / (xmax - xmin) * WIDTH;
                     let pixel_y = ymax * HEIGHT - (y - ymin) / (ymax - ymin) * HEIGHT;
